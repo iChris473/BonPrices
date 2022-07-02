a643870371514be83a2dcdab7788a01f90293161
iChris <christianjoseph473@gmail.com>
2022-07-02 16:00:27 +0100

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,12 +4,12 @@ import {LocationMarkerIcon} from '@heroicons/react/outline'
 
 export default function Footer() {
   return (
-    <div className='bg-gray-100 p-5 flex items-start justify-center flex-col gap-5'>
-        <div className='flex items-center justify-start border-b border-[#d6a7d2] pb-5 w-full'>
+    <div className='bg-gray-100 p-5 flex items-start justify-center flex-col gap-1'>
+        <div className='flex items-center justify-start border-b border-[#d6a7d2] pb-2 w-full'>
             <LocationMarkerIcon className='h-5 text-gray-500' />
-            <p className='font-bold text-gray-500 text-md'>Abuja, GwagwaLada</p>
+            <p className='font-bold text-gray-500 text-sm'>Abuja, GwagwaLada</p>
         </div>
-        <p className='font-semibold text-gray-700 ml-1'> &copy; Bon Maximus Company</p>
+        <p className='font-semibold text-gray-700 ml-1 text-sm'> &copy; Bon Maximus Company</p>
     </div>
   )
 }
