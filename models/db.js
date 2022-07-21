a99df5be8772898fe2de5a55ac069d5a6a20c689
iChris <christianjoseph473@gmail.com>
2022-07-21 13:25:07 +0100

    removed mongo  envs

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://ichris:uRltYApIKnACkOXK@cluster0.rpaco.mongodb.net/Goldstones?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
